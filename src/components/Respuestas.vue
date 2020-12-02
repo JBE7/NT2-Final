609eed1df821e961fc06ec1aea1f7361b7f383e1
Julian Brea <jbrea@baufest.com>
2020-12-01 21:00:01 -0300

    Final finalizado :D

---
+++ src/components/Respuestas.vue
@@ -0,0 +1,57 @@
+<template>
+     
+      <div class="table-responsive">
+          <table class="table">
+            <tr class="bg-success text-white">
+              <th>Número de pregunta</th>
+              <th>Respuesta</th>
+            </tr>
+            <tr
+              class="bg-info text-white"
+              v-for="(preg, index) in preguntas"
+              :key="index"
+            >
+              <td>{{ preg.pregunta }}</td>
+              <td><a v-for="(res, index) in preg.respuestas"
+              :key="index"> {{ res }}</a></td>
+            </tr>
+          </table>
+      </div>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-respuestas',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        preguntas : [
+          { pregunta: 1, respuestas: ["b"] },
+          { pregunta: 2, respuestas: ["c"] },
+          { pregunta: 3, respuestas: ["b"] },
+          { pregunta: 4, respuestas: ["d"] },
+          { pregunta: 5, respuestas: ["d"] },
+        ]
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-respuestas {
+
+  }
+</style>
---
+++ src/components/texto.vue
@@ -0,0 +1,71 @@
+<template>
+
+  <div class="jumbotron">
+    <div>
+      Color: <input type="text" v-model="color" />
+      Fondo: <input type="text" v-model="bkColor" />
+      Size: <input type="text" v-model="size" />
+    </div>
+
+    <hr>
+
+    <div>
+      Texto: <input type="text" v-model="texto" :style="{ 'color': color, 'fontSize': getSizeFormated(), 'backgroundColor': bkColor }" />
+     
+      <p v-if="contarPalabras.mostrar">
+        Cantidad de palabras: {{contarPalabras.cantidad}}
+      </p>
+    </div>
+
+  <br/>
+  <br/>
+  <hr>
+
+    <div>
+      <Respuestas> </Respuestas>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  import Respuestas from "./Respuestas"
+
+  export default  {
+    name: 'src-components-texto',
+    props: [],
+    mounted () {
+
+    },
+    components: {
+      Respuestas
+    },
+    data () {
+      return {
+        color: 'black',
+        bkColor: 'white',
+        size: '20',
+        texto: ''
+      }
+    },
+    methods: {
+      getSizeFormated(){
+        return this.size + 'px'
+      }
+    },
+    computed: {
+      contarPalabras(){
+        let cant = this.texto.split(' ').filter((a) => a != '').length 
+        return {
+          mostrar: cant > 0,
+          cantidad: cant 
+        }
+      }
+    }
+}
+</script>
+
+<style>
+  
+</style>
